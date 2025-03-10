3b426067d8a7b5c63d5dbbd9a2d4d1a783dffb0b
datta-nilotpal <163278549+datta-nilotpal@users.noreply.github.com>
2025-03-10 16:47:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.fonts.load('1em "Lavishly Yours"').then(function () {
+    // Load both fonts before making text visible
+    Promise.all([
+        document.fonts.load('1em "Poppins"'),
+        document.fonts.load('1em "Lavishly Yours"')
+    ]).then(function () {
         document.body.classList.add("font-loaded");
     });
 });
